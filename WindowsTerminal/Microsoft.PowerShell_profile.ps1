47f4231502d179c4e54dc887f5c934371ce796cd
ayuayue <1401262639@qq.com>
2020-10-15 17:27:55 +0800

    powershell style

--- WindowsTerminal/Microsoft.PowerShell_profile.ps1
+++ WindowsTerminal/Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,20 @@
+# 清除默认ctrl + r 搜索历史
+Remove-PSReadlineKeyHandler 'Ctrl+r'
 Import-Module posh-git
 Import-Module oh-my-posh
+Import-Module PSFzf
+Import-Module PoShFuck
 Set-Theme Paradox
 # Chocolatey profile
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
+function lsc {
+  Param ([int]$c = 5)
+  ls | Format-Wide -Column $c -Property Name
+}
+# tab 补全
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 # Setup other alias
 Set-Alias l	ls 
